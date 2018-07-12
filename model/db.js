0e59e20c03efcfad54e347273997a951a97753c9
voipnorm <chris.norman@hotmail.com>
2018-07-12 12:10:24 -0700

    Improved DB error reporting, package security fixes

--- model/db.js
+++ model/db.js
@@ -1,2 +1,12 @@
+var log = require('../svrConfig/logger');
 var mongoose = require('mongoose');
+var db = mongoose.connection;
 mongoose.connect('mongodb://localhost/presenceDB');
+
+
+db.on('error', function(err){
+    log.error("DB Connection Failure: "+err)
+});
+db.once('open', function() {
+   log.info('Successfully connected to MongoDB!!');
+});
