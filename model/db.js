0ec04611db71b490c7cc6bb95131c8b552e7dba9
voipnorm <chris.norman@hotmail.com>
2018-07-12 13:10:35 -0700

    Improved DB error reporting, package security fixes

--- model/db.js
+++ model/db.js
@@ -1,11 +1,13 @@
 var log = require('../svrConfig/logger');
+var myutils = require('../myutils/myutils');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 mongoose.connect('mongodb://localhost/presenceDB');
 
 
 db.on('error', function(err){
-    log.error("DB Connection Failure: "+err)
+    log.error("DB Connection Failure: "+err);
+    myutils.sparkPost("DB Connection Failure: "+err, process.env.SPARK_ROOM_ID)
 });
 db.once('open', function() {
    log.info('Successfully connected to MongoDB!!');
